c1332240062d112f5fad3b02950c4aaa1014e463
Olexander Tsyganok <zedicusgd@gmail.com>
2023-03-24 13:16:46 +0200

    add downloading files

--- src/Components/AccountPage/BuyedItemCard.js
+++ src/Components/AccountPage/BuyedItemCard.js
@@ -1,10 +1,32 @@
 import './AccountPage.css';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import fileDownload from 'js-file-download';
+
 const BuyedItemCard = (props) => {
 
 
     const downloadFile = (url, fileName) => {
-       
+        console.log(fileName);
+        /*fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
+          .then(res => res.blob())
+          .then(res => {
+            const aElement = document.createElement('a');
+            aElement.setAttribute('download', fileName);
+            const href = URL.createObjectURL(res);
+            aElement.href = href;
+            aElement.setAttribute('target', '_blank');
+            aElement.click();
+            URL.revokeObjectURL(href);
+          });*/
+          axios.get(url, {
+            responseType: 'blob',
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+            }
+          })
+          .then((res) => {
+            fileDownload(res.data, fileName)
+          })
       };
 
     return (
@@ -13,7 +35,6 @@ const BuyedItemCard = (props) => {
                 <div className="asset-details">
                     <h3 className="asset-title">{props.productName}</h3>
                     <button onClick={() => downloadFile(props.productLink,props.productName.replaceAll(' ', '_')+props.productLink.substring(props.productLink.lastIndexOf('.')))} className="download-button">Скачати</button>
-                    <Link to={props.productLink} target="_blank" download>Download</Link>
                 </div>
         </div>
 
