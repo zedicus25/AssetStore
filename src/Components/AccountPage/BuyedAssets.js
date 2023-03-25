b753e30c8786411a0f709609db4f28f2f1f36604
Olexander Tsyganok <zedicusgd@gmail.com>
2023-03-25 09:35:41 +0200

    add searching in buyed assets

--- src/Components/AccountPage/BuyedAssets.js
+++ src/Components/AccountPage/BuyedAssets.js
@@ -2,7 +2,7 @@ import UserNavigationPage from "./UserNavigationBar";
 import "./AccountPage.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getBuyedProducts, selectBuyedProducts } from "../../app/productsSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import token from './../../jwtToken';
 import { useNavigate } from "react-router-dom";
 import BuyedItemCard from "./BuyedItemCard";
@@ -10,6 +10,7 @@ import BuyedItemCard from "./BuyedItemCard";
 const BuyedAssets = () => {
 
     const buyedProducts = useSelector(selectBuyedProducts);
+    const [search, setSearch] = useState('');
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -25,7 +26,11 @@ const BuyedAssets = () => {
         <>
             <UserNavigationPage></UserNavigationPage>
             <div className="page buyedAsset-panel">
-                {buyedProducts?.map((x, idx) => { console.log(x);
+                <div className="search-buyed">
+                    <input className="search-input-buy" onChange={(e) => setSearch(e.target.value)} type='search' placeholder="Search assets"></input>
+                    
+                </div>
+                {buyedProducts?.filter(x => x.name.toLowerCase().includes(search.toLowerCase())).map((x, idx) => {
                     return <BuyedItemCard key={idx} productLink={x.linkToFile} productId={x.id} productName={x.name}  productImage={x.photo}></BuyedItemCard>
                 })}
             </div>
--- src/Components/AccountPage/BuyedItemCard.js
+++ src/Components/AccountPage/BuyedItemCard.js
@@ -6,36 +6,24 @@ const BuyedItemCard = (props) => {
 
 
     const downloadFile = (url, fileName) => {
-        console.log(fileName);
-        /*fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-          .then(res => res.blob())
-          .then(res => {
-            const aElement = document.createElement('a');
-            aElement.setAttribute('download', fileName);
-            const href = URL.createObjectURL(res);
-            aElement.href = href;
-            aElement.setAttribute('target', '_blank');
-            aElement.click();
-            URL.revokeObjectURL(href);
-          });*/
-          axios.get(url, {
+        axios.get(url, {
             responseType: 'blob',
-            headers:{
+            headers: {
                 'Access-Control-Allow-Origin': '*',
             }
-          })
-          .then((res) => {
-            fileDownload(res.data, fileName)
-          })
-      };
+        })
+            .then((res) => {
+                fileDownload(res.data, fileName)
+            })
+    };
 
     return (
         <div className="asset-card">
-            <img src={props.productImage} alt={props.productImage}/>
-                <div className="asset-details">
-                    <h3 className="asset-title">{props.productName}</h3>
-                    <button onClick={() => downloadFile(props.productLink,props.productName.replaceAll(' ', '_')+props.productLink.substring(props.productLink.lastIndexOf('.')))} className="download-button">Скачати</button>
-                </div>
+            <img src={props.productImage} alt={props.productImage} />
+            <div className="asset-details">
+                <h3 className="asset-title">{props.productName}</h3>
+                <button onClick={() => downloadFile(props.productLink, props.productName.replaceAll(' ', '_') + props.productLink.substring(props.productLink.lastIndexOf('.')))} className="download-button">Download</button>
+            </div>
         </div>
 
     );
